281253ffe9bde2b05d5a134defb5fab1f671d689
Wilbert Chen <wilbertchen2001@gmail.com>
2022-05-04 23:16:26 +0700

    feat: complete search feature and display movies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import Searchbar from "./components/Searchbar";
+import MovieCard from "./components/MovieCard";
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com?apikey=6baa6add";
 
 function App() {
-  return <div>App</div>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("Iron Man");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const res = await fetch(`${API_URL}&s=${title}`);
+    const data = await res.json();
+    setMovies(data.Search);
+  };
+
+  return (
+    <div className="app">
+      <h1>Movie Library</h1>
+
+      <Searchbar searchMovies={searchMovies} />
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie, key) => (
+            <MovieCard key={key} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,16 +1,22 @@
-import React from "react";
+import { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import "../styles/Searchbar.css";
 
-function Searchbar() {
+function Searchbar({ searchMovies }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div className="searchbar">
       <input
         placeholder="Search movies"
-        value="Spiderman"
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <SearchIcon onClick={() => {}} />
+      <SearchIcon
+        onClick={() => {
+          searchMovies(searchTerm);
+        }}
+      />
     </div>
   );
 }
