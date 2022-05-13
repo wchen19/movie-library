586fc9c0cd76b9190663e2bdad6d288b818d2368
Wilbert Chen <wilbertchen2001@gmail.com>
2022-05-13 20:18:54 +0700

    change movie api

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,26 +1,57 @@
-import React from "react";
-import "../styles/MovieCard.css";
+import React, { useEffect, useState } from 'react';
+import '../styles/MovieCard.css';
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w500/';
 
 function MovieCard({ movie }) {
+  const [date, setDate] = useState('');
+
+  const getDate = () => {
+    const mydate = new Date(movie.release_date);
+    const month = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ][mydate.getMonth()];
+    const str = `${mydate.getDate()} ${month} ${mydate.getFullYear()}`;
+    setDate(str);
+  };
+
+  useEffect(() => {
+    getDate();
+  }, []);
+
   return (
     <div>
-      <div className="movie">
-        <div className="movieYear">
-          <p>{movie.Year}</p>
+      <div className='movie'>
+        <div className='movieReleaseDate'>
+          <p>{date}</p>
         </div>
-        <div className="moviePoster">
+        <div className='moviePoster'>
           <img
             src={
-              movie.Poster !== "N/A"
-                ? movie.Poster
-                : "https://via.placeholder.com/400"
+              movie.poster_path !== 'N/A'
+                ? `${IMG_PATH + movie.poster_path}`
+                : 'https://via.placeholder.com/400'
             }
-            alt={movie.Title}
+            alt={movie.title}
           />
         </div>
-        <div className="movieInfo">
-          <span>{movie.Type}</span>
-          <h3>{movie.Title}</h3>
+        <div className='movieInfo'>
+          <span>{`Rating: ${movie.vote_average}`}</span>
+          <h3>{movie.title}</h3>
+          <div>
+            <button>See Detail</button>
+          </div>
         </div>
       </div>
     </div>
