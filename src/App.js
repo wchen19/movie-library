7181f370d4185c7fc750a57fb80708595593ea04
Wilbert Chen <wilbertchen2001@gmail.com>
2022-05-14 13:13:25 +0700

    add style web

--- src/App.js
+++ src/App.js
@@ -1,37 +1,73 @@
-import { useEffect, useState } from "react";
-import Searchbar from "./components/Searchbar";
-import MovieCard from "./components/MovieCard";
-import "./App.css";
-
-const API_URL = "https://www.omdbapi.com?apikey=6baa6add";
+import React, { useEffect, useState } from 'react';
+import Searchbar from './components/Searchbar';
+import MovieCard from './components/MovieCard';
+import { API_KEY, API_URL, movie } from './api/config';
+import './App.css';
+import MovieDetailModal from './components/MovieDetailModal';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [movieDetail, setMovieDetail] = useState({});
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
-    searchMovies("Iron Man");
+    popularMovies();
   }, []);
 
-  const searchMovies = async (title) => {
-    const res = await fetch(`${API_URL}&s=${title}`);
+  const searchMovieDetail = async (movieId) => {
+    const res = await fetch(
+      `${API_URL}${movie.detail}${movieId}?api_key=${API_KEY}`
+    );
     const data = await res.json();
-    setMovies(data.Search);
+    console.log(data);
+    setMovieDetail(data);
+    setShowModal(true);
+  };
+
+  const popularMovies = async () => {
+    const res = await fetch(`${API_URL}${movie.popular}?api_key=${API_KEY}`);
+    const data = await res.json();
+    setMovies(data.results);
+  };
+
+  const searchMovies = async (title) => {
+    const res = await fetch(
+      `${API_URL}${movie.search}?api_key=${API_KEY}&query=${title}`
+    );
+    const data = await res.json();
+    setMovies(data.result);
   };
 
   return (
-    <div className="app">
+    <div className='app'>
+      {/* <div style={showModal && 'display:block'}></div> */}
       <h1>Movie Library</h1>
-
       <Searchbar searchMovies={searchMovies} />
-
+      {showModal ? (
+        <MovieDetailModal
+          style={showModal ? { display: 'block' } : { display: 'none' }}
+          setShowModal={setShowModal}
+          movieDetail={movieDetail}
+        />
+      ) : (
+        <div> </div>
+      )}
       {movies?.length > 0 ? (
-        <div className="container">
-          {movies.map((movie, key) => (
-            <MovieCard key={key} movie={movie} />
+        <div
+          style={showModal ? { display: 'none' } : { display: 'flex' }}
+          className='container'
+        >
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              movie={movie}
+              // setMovieId={setMovieId}
+              searchMovieDetail={searchMovieDetail}
+            />
           ))}
         </div>
       ) : (
-        <div className="empty">
+        <div className='empty'>
           <h2>No movies found</h2>
         </div>
       )}
